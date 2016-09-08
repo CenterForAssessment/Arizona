7c086476b93c62f547c3979e34323cbe5c1b80a1
dbetebenner <dbetebenner@gmail.com>
2016-09-08 15:08:29 -0400

    Adding configs for content area by mode

---
+++ Content_Area_by_Mode/SGP_CONFIG/2016/ELA.R
@@ -0,0 +1,9 @@
+ELA_2016.config <- list(
+        ELA_CBT.2016 = list(
+                sgp.content.areas=rep('ELA_CBT', 2),
+                sgp.panel.years=c("2015", "2016"),
+                sgp.grade.sequences=list(c('3', '4'), c('4', '5'), c('5', '6'), c('6', '7'), c('7', '8'), c('8', '9'), c('9', '10'), c('10', '11'))),
+        ELA_PBT.2016 = list(
+                sgp.content.areas=rep('ELA_PBT', 2),
+                sgp.panel.years=c("2015", "2016"),
+                sgp.grade.sequences=list(c('3', '4'), c('4', '5'), c('5', '6'), c('6', '7'), c('7', '8'), c('8', '9'), c('9', '10'), c('10', '11'))))
---
+++ Content_Area_by_Mode/SGP_CONFIG/2016/MATHEMATICS.R
@@ -0,0 +1,17 @@
+MATHEMATICS_2016.config <- list(
+    MATHEMATICS.2016 = list(
+                sgp.content.areas=c`('MATHEMATICS', 'MATHEMATICS'),
+                sgp.panel.years=c('2015', '2016'),
+                sgp.grade.sequences=list(c('3', '4'), c('4', '5'), c('5', '6'), c('6', '7'), c('7', '8'))),
+	ALGEBRA_I.2016 = list(
+                sgp.content.areas=c('MATHEMATICS', 'ALGEBRA_I'),
+                sgp.panel.years=c('2015', '2016'),
+                sgp.grade.sequences=list(c('6', '7'), c('7', '8'), c('8', '9'))),
+	GEOMETRY.2016 = list(
+                sgp.content.areas=c('ALGEBRA_I', 'GEOMETRY'),
+                sgp.panel.years=c('2015', '2016'),
+                sgp.grade.sequences=list(c('7', '8'), c('8', '9'), c('9', '10'), c('10', '11'))),
+	ALGEBRA_II.2016 = list(
+                sgp.content.areas=c('GEOMETRY', 'ALGEBRA_I'),
+                sgp.panel.years=c('2015', '2016'),
+                sgp.grade.sequences=list(c('8', '9'), c('9', '10'), c('10', '11'))))
