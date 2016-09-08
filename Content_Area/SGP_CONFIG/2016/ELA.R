7f278c25ef5391e021458aca7d7c4ec9a8caa5e7
dbetebenner <dbetebenner@gmail.com>
2016-09-08 14:58:12 -0400

    Adding config scripts for 2016

---
+++ Content_Area/SGP_CONFIG/2016/ELA.R
@@ -0,0 +1,5 @@
+ELA_2016.config <- list(
+        ELA.2016 = list(
+                sgp.content.areas=rep('ELA', 2),
+                sgp.panel.years=c("2015", "2016"),
+                sgp.grade.sequences=list(c('3', '4'), c('4', '5'), c('5', '6'), c('6', '7'), c('7', '8'), c('8', '9'), c('9', '10'), c('10', '11'))))
