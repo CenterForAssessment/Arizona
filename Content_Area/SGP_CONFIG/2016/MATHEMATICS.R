349c506eac80332df59fe6fcf853b14ec7a956cb
dbetebenner <dbetebenner@gmail.com>
2016-09-08 15:01:15 -0400

    Adding config scripts for 2016

--- Content_Area/SGP_CONFIG/2016/MATHEMATICS.R
+++ Content_Area/SGP_CONFIG/2016/MATHEMATICS.R
@@ -1,6 +1,6 @@
 MATHEMATICS_2016.config <- list(
     MATHEMATICS.2016 = list(
-                sgp.content.areas=rep('MATHEMATICS', 2),
+                sgp.content.areas=c`('MATHEMATICS', 'MATHEMATICS'),
                 sgp.panel.years=c('2015', '2016'),
                 sgp.grade.sequences=list(c('3', '4'), c('4', '5'), c('5', '6'), c('6', '7'), c('7', '8'))),
 	ALGEBRA_I.2016 = list(
