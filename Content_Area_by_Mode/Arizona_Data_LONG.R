739fb9a4b7ed5949428d032a4b206e8e106d65aa
dbetebenner <dbetebenner@gmail.com>
2016-10-04 17:21:39 -0400

    Updating LONG data prep script

--- Content_Area_by_Mode/Arizona_Data_LONG.R
+++ Content_Area_by_Mode/Arizona_Data_LONG.R
@@ -1,6 +1,6 @@
 ##############################################################
 ###
-### Script for creating Arizona_Data_LONG
+### Script for creating Arizona_Data_LONG_1516
 ###
 ##############################################################
 
@@ -12,7 +12,8 @@ require(data.table)
 
 ### Load data
 
-Arizona_Data_LONG <- fread("Data/Base_Files/ArizonaData1516.txt", colClasses=rep("character", 12))
+Arizona_Data_LONG_1516 <- fread("Data/Base_Files/ArizonaData1516.txt", colClasses=rep("character", 12))
+Arizona_Data_LONG_1314 <- fread("Data/Base_Files/ArizonaData1314.txt", colClasses=rep("character", 10))
 
 
 ##########################################################
@@ -21,49 +22,63 @@ Arizona_Data_LONG <- fread("Data/Base_Files/ArizonaData1516.txt", colClasses=rep
 
 ## CONTENT_AREA
 
-Arizona_Data_LONG[CONTENT_AREA=="English Language Arts",CONTENT_AREA:="ELA"]
-Arizona_Data_LONG[CONTENT_AREA=="Mathematics",CONTENT_AREA:="MATHEMATICS"]
-Arizona_Data_LONG[AssessmentSubtestTitle=="Algebra I", CONTENT_AREA:="ALGEBRA_I"]
-Arizona_Data_LONG[AssessmentSubtestTitle=="Geometry", CONTENT_AREA:="GEOMETRY"]
-Arizona_Data_LONG[AssessmentSubtestTitle=="Algebra II", CONTENT_AREA:="ALGEBRA_II"]
-Arizona_Data_LONG[,CONTENT_AREA:=paste(CONTENT_AREA, TEST_MODE, sep="_")]
+Arizona_Data_LONG_1516[CONTENT_AREA=="English Language Arts",CONTENT_AREA:="ELA"]
+Arizona_Data_LONG_1516[CONTENT_AREA=="Mathematics",CONTENT_AREA:="MATHEMATICS"]
+Arizona_Data_LONG_1516[AssessmentSubtestTitle=="Algebra I", CONTENT_AREA:="ALGEBRA_I"]
+Arizona_Data_LONG_1516[AssessmentSubtestTitle=="Geometry", CONTENT_AREA:="GEOMETRY"]
+Arizona_Data_LONG_1516[AssessmentSubtestTitle=="Algebra II", CONTENT_AREA:="ALGEBRA_II"]
+Arizona_Data_LONG_1516[,CONTENT_AREA:=paste(CONTENT_AREA, TEST_MODE, sep="_")]
 
 ## GRADE
 
-setnames(Arizona_Data_LONG, "GRADE", "GRADE_ENROLLED")
-Arizona_Data_LONG[AssessmentSubtestTitle=="ELA Grade 3", GRADE:="3"]
-Arizona_Data_LONG[AssessmentSubtestTitle=="ELA Grade 4", GRADE:="4"]
-Arizona_Data_LONG[AssessmentSubtestTitle=="ELA Grade 5", GRADE:="5"]
-Arizona_Data_LONG[AssessmentSubtestTitle=="ELA Grade 6", GRADE:="6"]
-Arizona_Data_LONG[AssessmentSubtestTitle=="ELA Grade 7", GRADE:="7"]
-Arizona_Data_LONG[AssessmentSubtestTitle=="ELA Grade 8", GRADE:="8"]
-Arizona_Data_LONG[AssessmentSubtestTitle=="ELA Grade 9", GRADE:="9"]
-Arizona_Data_LONG[AssessmentSubtestTitle=="ELA Grade 10", GRADE:="10"]
-Arizona_Data_LONG[AssessmentSubtestTitle=="ELA Grade 11", GRADE:="11"]
-Arizona_Data_LONG[AssessmentSubtestTitle=="Mathematics Grade 3", GRADE:="3"]
-Arizona_Data_LONG[AssessmentSubtestTitle=="Mathematics Grade 4", GRADE:="4"]
-Arizona_Data_LONG[AssessmentSubtestTitle=="Mathematics Grade 5", GRADE:="5"]
-Arizona_Data_LONG[AssessmentSubtestTitle=="Mathematics Grade 6", GRADE:="6"]
-Arizona_Data_LONG[AssessmentSubtestTitle=="Mathematics Grade 7", GRADE:="7"]
-Arizona_Data_LONG[AssessmentSubtestTitle=="Mathematics Grade 8", GRADE:="8"]
-Arizona_Data_LONG[AssessmentSubtestTitle %in% c("Algebra I", "Algebra II", "Geometry"), GRADE:="EOCT"]
+setnames(Arizona_Data_LONG_1516, "GRADE", "GRADE_ENROLLED")
+Arizona_Data_LONG_1516[AssessmentSubtestTitle=="ELA Grade 3", GRADE:="3"]
+Arizona_Data_LONG_1516[AssessmentSubtestTitle=="ELA Grade 4", GRADE:="4"]
+Arizona_Data_LONG_1516[AssessmentSubtestTitle=="ELA Grade 5", GRADE:="5"]
+Arizona_Data_LONG_1516[AssessmentSubtestTitle=="ELA Grade 6", GRADE:="6"]
+Arizona_Data_LONG_1516[AssessmentSubtestTitle=="ELA Grade 7", GRADE:="7"]
+Arizona_Data_LONG_1516[AssessmentSubtestTitle=="ELA Grade 8", GRADE:="8"]
+Arizona_Data_LONG_1516[AssessmentSubtestTitle=="ELA Grade 9", GRADE:="9"]
+Arizona_Data_LONG_1516[AssessmentSubtestTitle=="ELA Grade 10", GRADE:="10"]
+Arizona_Data_LONG_1516[AssessmentSubtestTitle=="ELA Grade 11", GRADE:="11"]
+Arizona_Data_LONG_1516[AssessmentSubtestTitle=="Mathematics Grade 3", GRADE:="3"]
+Arizona_Data_LONG_1516[AssessmentSubtestTitle=="Mathematics Grade 4", GRADE:="4"]
+Arizona_Data_LONG_1516[AssessmentSubtestTitle=="Mathematics Grade 5", GRADE:="5"]
+Arizona_Data_LONG_1516[AssessmentSubtestTitle=="Mathematics Grade 6", GRADE:="6"]
+Arizona_Data_LONG_1516[AssessmentSubtestTitle=="Mathematics Grade 7", GRADE:="7"]
+Arizona_Data_LONG_1516[AssessmentSubtestTitle=="Mathematics Grade 8", GRADE:="8"]
+Arizona_Data_LONG_1516[AssessmentSubtestTitle %in% c("Algebra I", "Algebra II", "Geometry"), GRADE:="EOCT"]
 
 ## SCALE_SCORE
 
-Arizona_Data_LONG[,SCALE_SCORE:=as.numeric(SCALE_SCORE)]
+Arizona_Data_LONG_1314[,SCALE_SCORE:=as.numeric(SCALE_SCORE)]
+Arizona_Data_LONG_1314[SCALE_SCORE==0, SCALE_SCORE:=NA]
+Arizona_Data_LONG_1516[,SCALE_SCORE:=as.numeric(SCALE_SCORE)]
+
+## ACHIEVEMENT_LEVEL
+
+Arizona_Data_LONG_1314[ACHIEVEMENT_LEVEL=="", ACHIEVEMENT_LEVEL:=as.character(NA)]
 
 # ENROLLMENT_STATUS
 
-Arizona_Data_LONG[,STATE_ENROLLMENT_STATUS:=factor(1, levels=0:1, labels=c("Enrolled State: No", "Enrolled State: Yes"))]
-Arizona_Data_LONG[,DISTRICT_ENROLLMENT_STATUS:=as.factor(DISTRICT_ENROLLMENT_STATUS)]
-setattr(Arizona_Data_LONG[['DISTRICT_ENROLLMENT_STATUS']], "levels", c("Enrolled District: No", "Enrolled District: Yes"))
-Arizona_Data_LONG[,SCHOOL_ENROLLMENT_STATUS:=as.factor(SCHOOL_ENROLLMENT_STATUS)]
-setattr(Arizona_Data_LONG[['SCHOOL_ENROLLMENT_STATUS']], "levels", c("Enrolled School: No", "Enrolled School: Yes"))
+Arizona_Data_LONG_1314[,STATE_ENROLLMENT_STATUS:=factor(1, levels=0:1, labels=c("Enrolled State: No", "Enrolled State: Yes"))]
+Arizona_Data_LONG_1516[,STATE_ENROLLMENT_STATUS:=factor(1, levels=0:1, labels=c("Enrolled State: No", "Enrolled State: Yes"))]
+Arizona_Data_LONG_1314[,DISTRICT_ENROLLMENT_STATUS:=as.factor(DISTRICT_ENROLLMENT_STATUS)]
+Arizona_Data_LONG_1516[,DISTRICT_ENROLLMENT_STATUS:=as.factor(DISTRICT_ENROLLMENT_STATUS)]
+setattr(Arizona_Data_LONG_1314[['DISTRICT_ENROLLMENT_STATUS']], "levels", c("Enrolled District: No", "Enrolled District: Yes"))
+setattr(Arizona_Data_LONG_1516[['DISTRICT_ENROLLMENT_STATUS']], "levels", c("Enrolled District: No", "Enrolled District: Yes"))
+Arizona_Data_LONG_1314[,SCHOOL_ENROLLMENT_STATUS:=as.factor(SCHOOL_ENROLLMENT_STATUS)]
+setattr(Arizona_Data_LONG_1314[['SCHOOL_ENROLLMENT_STATUS']], "levels", c("Enrolled School: No", "Enrolled School: Yes"))
+Arizona_Data_LONG_1516[,SCHOOL_ENROLLMENT_STATUS:=as.factor(SCHOOL_ENROLLMENT_STATUS)]
+setattr(Arizona_Data_LONG_1516[['SCHOOL_ENROLLMENT_STATUS']], "levels", c("Enrolled School: No", "Enrolled School: Yes"))
+
+## rbind data
+
+Arizona_Data_LONG <- rbindlist(list(Arizona_Data_LONG_1314, Arizona_Data_LONG_1516), fill=TRUE)
 
 ## VALID_CASE
 
 Arizona_Data_LONG[,VALID_CASE:="VALID_CASE"]
-
 
 ### Resolve duplicates
 
