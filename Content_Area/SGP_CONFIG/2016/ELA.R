8ed73a3a74f943bfe18ae16ebd6bd4f204bf7fc8
dbetebenner <dbetebenner@gmail.com>
2016-11-08 04:36:59 -0500

    Update of ELA config

--- Content_Area/SGP_CONFIG/2016/ELA.R
+++ Content_Area/SGP_CONFIG/2016/ELA.R
@@ -1,6 +1,6 @@
 ELA_2016.config <- list(
         ELA.2016 = list(
-                sgp.content.areas=rep('ELA', 4),
+                sgp.content.areas=c('READING', 'READING', 'ELA', 'ELA'),
                 sgp.panel.years=c('2013', '2014', '2015', '2016'),
                 sgp.grade.sequences=list(c('3', '4'), c('3', '4', '5'), c('3', '4', '5', '6'), c('4', '5', '6', '7'), c('5', '6', '7', '8'), c('6', '7', '8', '9'),
                                             c('7', '8', '9', '10'), c('8', '9', '10', '11'))))
