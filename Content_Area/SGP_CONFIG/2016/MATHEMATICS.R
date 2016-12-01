0272239c7030a46f99ac2747210727226671dc2e
dbetebenner <dbetebenner@gmail.com>
2016-12-01 10:08:33 -0500

    Final updates to configuration

--- Content_Area/SGP_CONFIG/2016/MATHEMATICS.R
+++ Content_Area/SGP_CONFIG/2016/MATHEMATICS.R
@@ -1,9 +1,12 @@
+### MATHEMATICS
+
 MATHEMATICS_2016.config <- list(
     MATHEMATICS.2016 = list(
                 sgp.content.areas=c('MATHEMATICS', 'MATHEMATICS', 'MATHEMATICS', 'MATHEMATICS'),
                 sgp.panel.years=c('2013', '2014', '2015', '2016'),
                 sgp.grade.sequences=list(c('3', '4'), c('3', '4', '5'), c('3', '4', '5', '6'), c('4', '5', '6', '7'), c('5', '6', '7', '8')))
 )
+
 
 ### ALGEBRA I
 
@@ -20,6 +23,7 @@ ALGEBRA_I_2016.config <- list(
 		sgp.norm.group.preference=1)
 )
 
+
 ### GEOMETRY
 
 GEOMETRY_2016.config <- list(
@@ -28,72 +32,31 @@ GEOMETRY_2016.config <- list(
                 sgp.panel.years=c('2013', '2014', '2015', '2016'),
                 sgp.grade.sequences=list(c('5', '6', '7', 'EOCT'), c('6', '7', '8', 'EOCT')),
                 sgp.norm.group.preference=c(1,1)),
-#	GEOMETRY.2016 = list(
-#                sgp.content.areas=c('MATHEMATICS', 'ALGEBRA_I', 'GEOMETRY'),
-#                sgp.panel.years=c('2014', '2015', '2016'),
-#                sgp.grade.sequences=list(c('6', 'EOCT', 'EOCT')),
-#                sgp.exact.grade.progression=list(TRUE),
-#                sgp.norm.group.preference=2),
-#	GEOMETRY.2016 = list(
-#                sgp.content.areas=c('MATHEMATICS', 'ALGEBRA_I', 'GEOMETRY'),
-#                sgp.panel.years=c('2014', '2015', '2016'),
-#                sgp.grade.sequences=list(c('7', 'EOCT', 'EOCT')),
-#                sgp.exact.grade.progression=list(TRUE),
-#                sgp.norm.group.preference=2),
-#	GEOMETRY.2016 = list(
-#                sgp.content.areas=c('MATHEMATICS', 'ALGEBRA_I', 'GEOMETRY'),
-#                sgp.panel.years=c('2014', '2015', '2016'),
-#                sgp.grade.sequences=list(c('8', 'EOCT', 'EOCT')),
-#                sgp.exact.grade.progression=list(TRUE),
-#                sgp.norm.group.preference=2),
-#	GEOMETRY.2016 = list(
-#                sgp.content.areas=c('MATHEMATICS', 'ALGEBRA_I', 'GEOMETRY'),
-#                sgp.panel.years=c('2014', '2015', '2016'),
-#                sgp.grade.sequences=list(c('9', 'EOCT', 'EOCT')),
-#                sgp.exact.grade.progression=list(TRUE),
-#                sgp.norm.group.preference=2),
-#	GEOMETRY.2016 = list(
-#                sgp.content.areas=c('MATHEMATICS', 'ALGEBRA_I', 'GEOMETRY'),
-#                sgp.panel.years=c('2014', '2015', '2016'),
-#                sgp.grade.sequences=list(c('10', 'EOCT', 'EOCT')),
-#                sgp.exact.grade.progression=list(TRUE),
-#                sgp.norm.group.preference=2),
 	GEOMETRY.2016 = list(
                 sgp.content.areas=c('MATHEMATICS', 'MATHEMATICS', 'ALGEBRA_I', 'GEOMETRY'),
                 sgp.panel.years=c('2013', '2014', '2015', '2016'),
                 sgp.grade.sequences=list(c('5', '6', 'EOCT', 'EOCT')),
-#                sgp.exact.grade.progression=list(TRUE),
                 sgp.norm.group.preference=1),
 	GEOMETRY.2016 = list(
                 sgp.content.areas=c('MATHEMATICS', 'MATHEMATICS', 'ALGEBRA_I', 'GEOMETRY'),
                 sgp.panel.years=c('2013', '2014', '2015', '2016'),
                 sgp.grade.sequences=list(c('6', '7', 'EOCT', 'EOCT')),
-#                sgp.exact.grade.progression=list(TRUE),
                 sgp.norm.group.preference=1),
 	GEOMETRY.2016 = list(
                 sgp.content.areas=c('MATHEMATICS', 'MATHEMATICS', 'ALGEBRA_I', 'GEOMETRY'),
                 sgp.panel.years=c('2013', '2014', '2015', '2016'),
                 sgp.grade.sequences=list(c('7', '8', 'EOCT', 'EOCT')),
-#                sgp.exact.grade.progression=list(TRUE),
                 sgp.norm.group.preference=1),
 	GEOMETRY.2016 = list(
                 sgp.content.areas=c('MATHEMATICS', 'MATHEMATICS', 'ALGEBRA_I', 'GEOMETRY'),
                 sgp.panel.years=c('2013', '2014', '2015', '2016'),
                 sgp.grade.sequences=list(c('8', '9', 'EOCT', 'EOCT')),
-#                sgp.exact.grade.progression=list(TRUE),
                 sgp.norm.group.preference=1),
 	GEOMETRY.2016 = list(
                 sgp.content.areas=c('MATHEMATICS', 'MATHEMATICS', 'ALGEBRA_I', 'GEOMETRY'),
                 sgp.panel.years=c('2013', '2014', '2015', '2016'),
                 sgp.grade.sequences=list(c('9', '10', 'EOCT', 'EOCT')),
-#                sgp.exact.grade.progression=list(TRUE),
                 sgp.norm.group.preference=1),
-#	GEOMETRY.2016 = list(
-#                sgp.content.areas=c('ALGEBRA_I', 'GEOMETRY'),
-#                sgp.panel.years=c('2015', '2016'),
-#                sgp.grade.sequences=list(c('EOCT', 'EOCT')),
-#                sgp.exact.grade.progression=list(TRUE),
-#                sgp.norm.group.preference=3),
 	GEOMETRY.2016 = list(
                 sgp.content.areas=c('ALGEBRA_II', 'GEOMETRY'),
                 sgp.panel.years=c('2015', '2016'),
@@ -106,60 +69,26 @@ GEOMETRY_2016.config <- list(
 ### ALGEBRA II
 
 ALGEBRA_II_2016.config <- list(
-#	ALGEBRA_II.2016 = list(
-#                sgp.content.areas=c('MATHEMATICS', 'GEOMETRY', 'ALGEBRA_II'),
-#                sgp.panel.years=c('2014', '2015', '2016'),
-#                sgp.grade.sequences=list(c('7', 'EOCT', 'EOCT')),
-#                sgp.exact.grade.progression=list(TRUE),
-#                sgp.norm.group.preference=2),
-#	ALGEBRA_II.2016 = list(
-#                sgp.content.areas=c('MATHEMATICS', 'GEOMETRY', 'ALGEBRA_II'),
-#                sgp.panel.years=c('2014', '2015', '2016'),
-#                sgp.grade.sequences=list(c('8', 'EOCT', 'EOCT')),
-#                sgp.exact.grade.progression=list(TRUE),
-#                sgp.norm.group.preference=2),
-#	ALGEBRA_II.2016 = list(
-#                sgp.content.areas=c('MATHEMATICS', 'GEOMETRY', 'ALGEBRA_II'),
-#                sgp.panel.years=c('2014', '2015', '2016'),
-#                sgp.grade.sequences=list(c('9', 'EOCT', 'EOCT')),
-#                sgp.exact.grade.progression=list(TRUE),
-#                sgp.norm.group.preference=2),
-#	ALGEBRA_II.2016 = list(
-#                sgp.content.areas=c('MATHEMATICS', 'GEOMETRY', 'ALGEBRA_II'),
-#                sgp.panel.years=c('2014', '2015', '2016'),
-#                sgp.grade.sequences=list(c('10', 'EOCT', 'EOCT')),
-#                sgp.exact.grade.progression=list(TRUE),
-#                sgp.norm.group.preference=2),
 	ALGEBRA_II.2016 = list(
                 sgp.content.areas=c('MATHEMATICS', 'MATHEMATICS', 'GEOMETRY', 'ALGEBRA_II'),
                 sgp.panel.years=c('2013', '2014', '2015', '2016'),
                 sgp.grade.sequences=list(c('6', '7', 'EOCT', 'EOCT')),
-#                sgp.exact.grade.progression=list(TRUE),
                 sgp.norm.group.preference=1),
 	ALGEBRA_II.2016 = list(
                 sgp.content.areas=c('MATHEMATICS', 'MATHEMATICS', 'GEOMETRY', 'ALGEBRA_II'),
                 sgp.panel.years=c('2013', '2014', '2015', '2016'),
                 sgp.grade.sequences=list(c('7', '8', 'EOCT', 'EOCT')),
-#                sgp.exact.grade.progression=list(TRUE),
                 sgp.norm.group.preference=1),
 	ALGEBRA_II.2016 = list(
                 sgp.content.areas=c('MATHEMATICS', 'MATHEMATICS', 'GEOMETRY', 'ALGEBRA_II'),
                 sgp.panel.years=c('2013', '2014', '2015', '2016'),
                 sgp.grade.sequences=list(c('8', '9', 'EOCT', 'EOCT')),
-#                sgp.exact.grade.progression=list(TRUE),
                 sgp.norm.group.preference=1),
 	ALGEBRA_II.2016 = list(
                 sgp.content.areas=c('MATHEMATICS', 'MATHEMATICS', 'GEOMETRY', 'ALGEBRA_II'),
                 sgp.panel.years=c('2013', '2014', '2015', '2016'),
                 sgp.grade.sequences=list(c('9', '10', 'EOCT', 'EOCT')),
-#                sgp.exact.grade.progression=list(TRUE),
                 sgp.norm.group.preference=1),
-#	ALGEBRA_II.2016 = list(
-#                sgp.content.areas=c('GEOMETRY', 'ALGEBRA_II'),
-#                sgp.panel.years=c('2015', '2016'),
-#                sgp.grade.sequences=list(c('EOCT', 'EOCT')),
-#                sgp.exact.grade.progression=list(TRUE),
-#                sgp.norm.group.preference=3),
 	ALGEBRA_II.2016 = list(
                 sgp.content.areas=c('ALGEBRA_I', 'ALGEBRA_II'),
                 sgp.panel.years=c('2015', '2016'),
