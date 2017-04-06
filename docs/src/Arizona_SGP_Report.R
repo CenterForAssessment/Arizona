ee67deb5055a9208a8ccd0157761e8f0c07a4dd8
dbetebenner <dbetebenner@gmail.com>
2017-04-06 16:33:59 -0400

    Update

--- docs/src/Arizona_SGP_Report.R
+++ docs/src/Arizona_SGP_Report.R
@@ -4,8 +4,8 @@
 ###
 #######################################################################
 
-knitr::knit("Arizona_Report.Rmd")
-rmarkdown::render("Arizona_Report.md", output_format="all")
-file.rename("Arizona_Report.html", file.path("..", "index.html"))
-file.rename("Arizona_Report.pdf", file.path("..", "Arizona_Report.pdf"))
-unlink("Arizona_Report.md")
+knitr::knit("Arizona_SGP_Report.Rmd")
+rmarkdown::render("Arizona_SGP_Report.md", output_format="all")
+file.rename("Arizona_SGP_Report.html", file.path("..", "Arizona_SGP_Report.html"))
+file.rename("Arizona_SGP_Report.pdf", file.path("..", "Arizona_SGP_Report.pdf"))
+unlink("Arizona_SGP_Report.md")
