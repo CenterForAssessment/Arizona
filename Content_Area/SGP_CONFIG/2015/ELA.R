1c1ae9d6f4fec50ccd1e9fbb22cea701544a1a4c
dbetebenner <dbetebenner@gmail.com>
2017-05-08 11:53:53 -0400

    Update of Arizona

--- Content_Area/SGP_CONFIG/2015/ELA.R
+++ Content_Area/SGP_CONFIG/2015/ELA.R
@@ -2,6 +2,9 @@ ELA_2015.config <- list(
         ELA.2015 = list(
                 sgp.content.areas=c('READING', 'READING', 'ELA'),
                 sgp.panel.years=c('2013', '2014', '2015'),
-                sgp.grade.sequences=list(c('3', '4'), c('3', '4', '5'), c('4', '5', '6'), c('5', '6', '7'), c('6', '7', '8'), c('7', '8', '9'),
+                sgp.grade.sequences=list(c('3', '4'), c('3', '4', '5'), c('4', '5', '6'),
+                                            c('5', '6', '7'), c('6', '7', '8'), c('7', '8', '9'),
                                             c('8', '9', '10'), c('9', '10', '11')),
-		sgp.projection.grade.sequences=list("NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS")))
+		        sgp.projection.grade.sequences=list("NO_PROJECTIONS", "NO_PROJECTIONS",
+                                        "NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS",
+                                        "NO_PROJECTIONS", "NO_PROJECTIONS", "NO_PROJECTIONS")))
--- Content_Area/SGP_CONFIG/2016/ELA.R
+++ Content_Area/SGP_CONFIG/2016/ELA.R
@@ -4,7 +4,8 @@ ELA_2016.config <- list(
                 sgp.projection.content.areas=c('ELA', 'ELA'),
                 sgp.panel.years=c('2013', '2014', '2015', '2016'),
                 sgp.projection.panel.years=c('2015', '2016'),
-                sgp.grade.sequences=list(c('3', '4'), c('3', '4', '5'), c('3', '4', '5', '6'), c('4', '5', '6', '7'), c('5', '6', '7', '8'), c('6', '7', '8', '9'),
+                sgp.grade.sequences=list(c('3', '4'), c('3', '4', '5'), c('3', '4', '5', '6'),
+                                            c('4', '5', '6', '7'), c('5', '6', '7', '8'), c('6', '7', '8', '9'),
                                             c('7', '8', '9', '10'), c('8', '9', '10', '11')),
 		sgp.projection.grade.sequences=list('3', '4', '5', '6', '7', '8', '9', '10'),
 		sgp.projection.sequence="ELA")
