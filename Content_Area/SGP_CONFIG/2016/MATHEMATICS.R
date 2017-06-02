bbcab925e7eab8c6e3804c92c0905c0cb786f0b9
dbetebenner <dbetebenner@gmail.com>
2017-06-02 17:01:23 -0400

    Updating 2016 Math configs

--- Content_Area/SGP_CONFIG/2016/MATHEMATICS.R
+++ Content_Area/SGP_CONFIG/2016/MATHEMATICS.R
@@ -3,9 +3,12 @@
 MATHEMATICS_2016.config <- list(
     MATHEMATICS.2016 = list(
                 sgp.content.areas=c('MATHEMATICS', 'MATHEMATICS', 'MATHEMATICS', 'MATHEMATICS'),
+                sgp.projection.content.areas=c('MATHEMATICS', 'MATHEMATICS'),
                 sgp.panel.years=c('2013', '2014', '2015', '2016'),
+                sgp.projection.panel.years=c('2015', '2016'),
                 sgp.grade.sequences=list(c('3', '4'), c('3', '4', '5'), c('3', '4', '5', '6'),
                                         c('4', '5', '6', '7'), c('5', '6', '7', '8')))
+                sgp.projection.grade.sequences=list('3', '4', '5', '6', '7', '8', '9', '10'),
 )
 
 
