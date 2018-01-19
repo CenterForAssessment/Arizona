70cc5c874c111f32c55bda780dd9cb09e3a7775b
dbetebenner <dbetebenner@gmail.com>
2018-01-19 11:27:59 -0500

    Updating MATHEMATICS configs with new projection sequence

--- Content_Area/SGP_CONFIG/2017/MATHEMATICS.R
+++ Content_Area/SGP_CONFIG/2017/MATHEMATICS.R
@@ -6,7 +6,7 @@ MATHEMATICS_2017.config <- list(
                  sgp.content.areas=c('MATHEMATICS', 'MATHEMATICS', 'MATHEMATICS'),
                  sgp.panel.years=c('2015', '2016', '2017'),
                  sgp.grade.sequences=list(c('3', '4'), c('3', '4', '5'), c('4', '5', '6'), c('5', '6', '7'), c('6', '7', '8')),
-                 sgp.projection.sequence="MATHEMATICS")
+                 sgp.projection.sequence=c("MATHEMATICS", "ALGEBRA_I_FROM_7"))
 )
 
 
