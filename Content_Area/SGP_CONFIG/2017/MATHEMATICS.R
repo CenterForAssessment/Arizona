160c96d556253211155e0115222762f4adf20c1e
dbetebenner <dbetebenner@gmail.com>
2018-01-22 11:20:17 -0500

    Fixing up configuration

--- Content_Area/SGP_CONFIG/2017/MATHEMATICS.R
+++ Content_Area/SGP_CONFIG/2017/MATHEMATICS.R
@@ -5,8 +5,13 @@ MATHEMATICS_2017.config <- list(
      MATHEMATICS.2017 = list(
                  sgp.content.areas=c('MATHEMATICS', 'MATHEMATICS', 'MATHEMATICS'),
                  sgp.panel.years=c('2015', '2016', '2017'),
-                 sgp.grade.sequences=list(c('3', '4'), c('3', '4', '5'), c('4', '5', '6'), c('5', '6', '7'), c('6', '7', '8')),
+                 sgp.grade.sequences=list(c('3', '4'), c('3', '4', '5'), c('4', '5', '6'), c('5', '6', '7')),
                  sgp.projection.sequence=c("MATHEMATICS", "ALGEBRA_I_FROM_7"))
+     MATHEMATICS.2017 = list(
+                 sgp.content.areas=c('MATHEMATICS', 'MATHEMATICS', 'MATHEMATICS'),
+                 sgp.panel.years=c('2015', '2016', '2017'),
+                 sgp.grade.sequences=list(c('6', '7', '8')),
+                 sgp.projection.sequence=c("MATHEMATICS"))
 )
 
 
