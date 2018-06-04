c9111af675f9095d796713a70662900bb902d6a8
dbetebenner <dbetebenner@gmail.com>
2018-06-04 13:58:06 -0400

    Adding in 2018 configs

---
+++ Content_Area/SGP_CONFIG/2018/ELA.R
@@ -0,0 +1,11 @@
+### ELA_2017
+
+ELA_2018.config <- list(
+         ELA.2018 = list(
+                 sgp.content.areas=c('ELA', 'ELA', 'ELA'),
+                 sgp.panel.years=c('2016', '2017', '2018'),
+                 sgp.grade.sequences=list(c('3', '4'), c('3', '4', '5'), c('4', '5', '6'),
+                                             c('5', '6', '7'), c('6', '7', '8'), c('7', '8', '9'),
+                                             c('8', '9', '10'), c('9', '10', '11')),
+                 sgp.projection.sequence="ELA")
+)
