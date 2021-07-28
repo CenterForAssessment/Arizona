929eed7874015dfac2a2279b96a07f83ad675d2b
Damian Betebenner <dbetebenner@nciea.org>
2021-07-28 14:35:35 -0400

    Removing an extra-parenthesis in the B and C scrip ts

--- Arizona_SGP_2021_PART_C.R
+++ Arizona_SGP_2021_PART_C.R
@@ -9,7 +9,7 @@ require(SGP)
 require(SGPmatrices)
 
 ###   Load data
-load("Data/Arizona_SGP.Rdata"))
+load("Data/Arizona_SGP.Rdata")
 
 ###   Load configurations
 source("SGP_CONFIG/2021/PART_C/ELA.R")
