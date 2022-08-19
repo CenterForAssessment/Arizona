1312945d7e42ab3d5a33967aacca6def196676b1
Damian Betebenner <dbetebenner@nciea.org>
2022-08-18 22:12:09 -0400

    Update of 2022 script

--- Arizona_SGP_2022_PART_B.R
+++ Arizona_SGP_2022_PART_B.R
@@ -26,17 +26,36 @@ AZ_CONFIG_BASELINE <- c(ELA_2022_BASELINE.config, MATHEMATICS_2022_BASELINE.conf
 parallel.config <- list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=4, BASELINE_PERCENTILES=4, PROJECTIONS=4, LAGGED_PROJECTIONS=4, SGP_SCALE_SCORE_TARGETS=4))
 
 #####
-###   Run updateSGP analysis
+###   Run cohort-referenced updateSGP analysis
 #####
 
 Arizona_SGP <- updateSGP(
         what_sgp_object = Arizona_SGP,
         with_sgp_data_LONG = Arizona_Data_LONG_2022,
-        steps = c("prepareSGP", "analyzeSGP", "combineSGP", "outputSGP"),
-        sgp.config = NH_CONFIG,
+        steps = c("prepareSGP", "analyzeSGP", "combineSGP"),
+        sgp.config = AZ_CONFIG,
         sgp.percentiles = TRUE,
         sgp.projections = TRUE,
         sgp.projections.lagged = TRUE,
+        sgp.percentiles.baseline = FALSE,
+        sgp.projections.baseline = FALSE,
+        sgp.projections.lagged.baseline = FALSE,
+        save.intermediate.results = FALSE,
+        parallel.config = parallel.config
+)
+
+
+#####
+###   Run baseline-referenced updateSGP analysis
+#####
+
+Arizona_SGP <- abcSGP(
+        sgp_object = Arizona_SGP,
+        steps = c("prepareSGP", "analyzeSGP", "combineSGP", "outputSGP"),
+        sgp.config = AZ_CONFIG_BASELINE,
+        sgp.percentiles = FALSE,
+        sgp.projections = FALSE,
+        sgp.projections.lagged = FALSE,
         sgp.percentiles.baseline = TRUE,
         sgp.projections.baseline = TRUE,
         sgp.projections.lagged.baseline = TRUE,
@@ -44,6 +63,5 @@ Arizona_SGP <- updateSGP(
         parallel.config = parallel.config
 )
 
-
 ###   Save results
-save(Arizona_SGP, file="Data/Arizona_SGP.Rdata")
+#save(Arizona_SGP, file="Data/Arizona_SGP.Rdata")
