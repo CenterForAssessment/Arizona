2ef561532a660a4fd47cadbe50a9df801a907f95
Damian Betebenner <dbetebenner@nciea.org>
2022-08-26 22:12:38 -0400

    Fixing up configurations

--- SGP_CONFIG/2022/PART_B/ELA.R
+++ SGP_CONFIG/2022/PART_B/ELA.R
@@ -8,7 +8,7 @@ ELA_2022.config <- list(
 	ELA_2022 = list(
 		sgp.content.areas=rep("ELA", 2),
 		sgp.panel.years=c("2021", "2022"),
-		sgp.grade.sequences=list(c("3", "4"), c("4", "5"), c("5", "6"), c("6", "7"), c("7", "8"),)),
+		sgp.grade.sequences=list(c("3", "4"), c("4", "5"), c("5", "6"), c("6", "7"), c("7", "8"))),
 	ELA_2022 = list(
 		sgp.content.areas=rep("ELA", 2),
 		sgp.panel.years=c("2021", "2022"),
