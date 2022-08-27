06c53919ca17b90526f21a79d67b2317a4f75699
Damian Betebenner <dbetebenner@nciea.org>
2022-08-26 22:07:14 -0400

    Fixing up configurations

--- SGP_CONFIG/2022/PART_B/MATHEMATICS.R
+++ SGP_CONFIG/2022/PART_B/MATHEMATICS.R
@@ -8,7 +8,7 @@ MATHEMATICS_2022.config <- list(
 	MATHEMATICS_2022 = list(
 		sgp.content.areas=rep("MATHEMATICS", 2),
 		sgp.panel.years=c("2021", "2022"),
-		sgp.grade.sequences=list(c("3", "4"), c("4", "5"), c("5", "6"), c("6", "7"), c("7", "8"), c("8", "9"), c("10", "11"))),
+		sgp.grade.sequences=list(c("3", "4"), c("4", "5"), c("5", "6"), c("6", "7"), c("7", "8"))),
 	MATHEMATICS_2022 = list(
 		sgp.content.areas=rep("MATHEMATICS", 2),
 		sgp.panel.years=c("2021", "2022"),
